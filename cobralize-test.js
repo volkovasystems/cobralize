ce7da17192429436369298618d13e2e4240900a0
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-09 13:09:08 +0800

    Updated test.

--- cobralize-test.js
+++ cobralize-test.js
@@ -2,12 +2,12 @@
 const assert = require( "assert" );
 const cobralize = require( "./cobralize.js" );
 
-assert.equal( cobralize( "Hello World" ), "HELLO_WORLD", "should have value 'HELLO_WORLD'" );
+assert.equal( cobralize( "Hello World" ), "HELLO_WORLD", "should be equal to 'HELLO_WORLD'" );
 
-assert.equal( cobralize( "hello_world" ), "HELLO_WORLD", "should have value 'HELLO_WORLD'" );
+assert.equal( cobralize( "hello_world" ), "HELLO_WORLD", "should be equal to 'HELLO_WORLD'" );
 
-assert.equal( cobralize( "helloWorld" ), "HELLO_WORLD", "should have value 'HELLO_WORLD'" );
+assert.equal( cobralize( "helloWorld" ), "HELLO_WORLD", "should be equal to 'HELLO_WORLD'" );
 
-assert.equal( cobralize( "hello world" ), "HELLO_WORLD", "should have value 'HELLO_WORLD'" );
+assert.equal( cobralize( "hello world" ), "HELLO_WORLD", "should be equal to 'HELLO_WORLD'" );
 
 console.log( "ok" );
